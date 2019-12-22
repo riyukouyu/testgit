dc80b3a7b40211bda57087e77c750d9d9c65afff
luoqingsheng <luoqingsheng@sunline.cn>
2019-12-22 15:10:09 +0800

    test

--- dptran/src/main/java/cn/sunline/ltts/busi/catran/trans/qt/insacc.java
+++
@@ -1,9 +0,0 @@
-package cn.sunline.ltts.busi.catran.trans.qt;
-
-
-public class insacc {
-
-public static void beforeInsacc( final cn.sunline.ltts.busi.catran.trans.qt.intf.Insacc.Input input){
-	
-}
-}
--- dptran/src/main/java/cn/sunline/ltts/busi/catran/trans/qt/updmac.java
+++
@@ -1,9 +0,0 @@
-package cn.sunline.ltts.busi.catran.trans.qt;
-
-
-public class updmac {
-
-public static void beforeUpdmac( final cn.sunline.ltts.busi.catran.trans.qt.intf.Updmac.Input input){
-	
-}
-}
